c95a8b68399e270428e84990ee346d6bf9635449
Gustavo Mezzovilla <gustavo.mezzovilla@gmail.com>
2023-01-20 23:15:10 -0300

    recursividade implantada

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <math.h>
-
 /* Testes:
 1 Inserir uma letra e deletá-la imediatamente
 2 Mover o cursor para frente e para trás diversas vezes
@@ -14,11 +13,70 @@
 7 Inserir caracteres especiais ou números para verificar se o código está lidando corretamente com esses casos.
 */
 
+
+// Funcao para limpar a tela 
+/*****************************/
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+void ClearScreen(){
+  HANDLE                     hStdOut;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD                      count;
+  DWORD                      cellCount;
+  COORD                      homeCoords = { 0, 0 };
+
+  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+  if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+  /* Get the number of cells in the current buffer */
+  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+  /* Fill the entire buffer with spaces */
+  if (!FillConsoleOutputCharacter(
+    hStdOut,
+    (TCHAR) ' ',
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Fill the entire buffer with the current colors and attributes */
+  if (!FillConsoleOutputAttribute(
+    hStdOut,
+    csbi.wAttributes,
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Move the cursor home */
+  SetConsoleCursorPosition( hStdOut, homeCoords );
+}
+
+#else // !_WIN32
+#include <unistd.h>
+#include <term.h>
+
+void ClearScreen(){
+  if (!cur_term)
+  {
+     int result;
+     setupterm( NULL, STDOUT_FILENO, &result );
+     if (result <= 0) return;
+  }
+
+   putp( tigetstr( "clear" ) );
+}
+#endif
+/*****************************/
+
 #define cap_local 64
 
 // funcoes implementadas
 #define NUM_FUNCOES 6
-char lista_f[NUM_FUNCOES] = {'Z', 'I', 'D', 'F', 'T', '!'};
+char lista_f[NUM_FUNCOES] = {'G', 'I', 'D', 'F', 'T', '!'};
 
 bool check_vec(char x, int i) {
     if (x == '\0')
@@ -31,7 +89,8 @@ bool check_vec(char x, int i) {
     return false;
 }
 
-// Funcoes associadas ao input local do usuario (que fica no console)
+// Variaveis globais
+/*****************************/
 typedef struct CONSOLE{
     char *letraz;
     int tamanho;
@@ -54,6 +113,8 @@ celula *head, *tail;
 
 int tipo_de_cursor = 0;
 
+// Funcoes do console
+/*****************************/
 void build_console(console* v){
     v->tamanho = 0;
     v->letraz = (char*) malloc(cap_local * sizeof(char));
@@ -80,6 +141,18 @@ void reseta_console(console* v){
     v->tamanho = 0;
 }
 
+void delete_char_console(console* cons, int i) {
+    if (i < 0 || i >= cons->tamanho)
+        return;
+
+    for (int j = i; j < cons->tamanho - 1; j++)
+        cons->letraz[j] = cons->letraz[j + 1];
+
+    cons->tamanho--;
+}
+
+// Funcoes da celula
+/*****************************/
 void build_celula(celula* new){
     new->val = '\0';
     new->next = NULL;
@@ -222,7 +295,7 @@ void imprime_coord(){
 }
 
 int number_no_console(console* d){
-    if(d->letraz[1] == '\0')
+    if(d->letraz[1] == '\0' || d->tamanho == 1)
         return 1;
     
     int S = 0;
@@ -237,55 +310,104 @@ int number_no_console(console* d){
     return S;
 }
 
+void move_cursor_next_palavra(){
+    while(cursor->cel != NULL && 
+          cursor->cel->val != ' ')
+        if(cursor->cel->next != NULL)
+            frente();
+}
+
+void move_cursor_prev_palavra(){
+    while(cursor->cel != NULL && 
+          cursor->cel->val != ' ')
+        if(cursor->cel->prev != NULL)
+            traz();
+}
+
 int parse(celula* cel, console* cons) {
+    ClearScreen();
+
     // pega o primeiro caractere da string
     char c = cons->letraz[0];
 
     int n = cons->tamanho;
-    int iteradas;
     
+    // numero de iteracoes de uma dada funcao
+    int iteradas = number_no_console(cons);
+
+    // uma vez que o numero de iteradas da funcao ja esta aramazenado, removemos ele do console
+    int j=0;
+    while(isdigit(cons->letraz[j+1])){
+        delete_char_console(cons, j+1);
+        j++;
+    }
+
+    if(iteradas == 0)
+        iteradas = 1;
+    
+    // lista de celulas auxiliar removendo a cabeca
     console *aux = malloc(sizeof(console));
     build_console(aux);
-    switch (c) {
-    case 'Z':
-        tipo_de_cursor= (tipo_de_cursor+1) % 2; 
-    case 'I':
-        // lista de celulas auxiliar removendo a cabeca
-        aux->tamanho = n - 1;
-        for(int i=0; i< n-1; i++)
-            aux->letraz[i] = cons->letraz[i+1];
+    aux->tamanho = n - 1;
+    for(int i=0; i< n-1; i++)
+        aux->letraz[i] = cons->letraz[i+1];
 
-	    from_console_to_line(aux);
-	    break;
-    case 'D':
-        if(cursor->cel != head)
-            delete_char();
-    case 'F': //move cursor pra frente
-        iteradas = 
-          number_no_console(cons);
-        while(iteradas > 0){ 
-            if((cursor->cel)->next != NULL)
+    switch (c) {
+        case 'G':
+            tipo_de_cursor= (tipo_de_cursor+1) % 2; 
+        case 'I':
+            from_console_to_line(aux);
+            break;
+        case 'D':
+            while(iteradas > 0){ 
+                if(cursor->cel != head)
+                    delete_char();
+                iteradas--;
+            }
+            break;
+        case 'F': //move cursor pra frente
+            while(iteradas > 0){ 
+                if((cursor->cel)->next != NULL)
+                    frente();
+                iteradas--;
+            }
+            break;
+        case 'T': //move cursor pra traz
+            while(iteradas > 0){ 
+                if((cursor->cel)->prev != NULL)
+                    traz();
+                iteradas--;
+            }
+            break;
+        case 'O':
+            if(cursor->cel != head){
+                cursor->cel = head;
+                cursor->coluna = 0;
+            }
+            break;
+        case '$':
+            while(cursor->cel != tail)
                 frente();
-            iteradas--;
-        }
-	    break;
-    case 'T': //move cursor pra traz
-        iteradas = 
-          number_no_console(cons);
-        
-        while(iteradas > 0){ 
-            if((cursor->cel)->prev != NULL)
-                traz();
-            iteradas--;
-        }
-        break;
-    case '!': // sair do programa
-        return 2;
-    case '\0':
-        break;
-    default:
-    printf("Comando desconhecido\n");
-    break;
+            break;
+        case 'P':
+            while(iteradas > 0){ 
+                move_cursor_next_palavra();
+                iteradas--;
+            }
+            break;    
+        case 'Q':
+            while(iteradas > 0){ 
+                move_cursor_prev_palavra();
+                iteradas--;
+            }
+            break;    
+        case '!': // sair do programa
+            return 2;
+        case '\0':
+            break;
+        default:
+            printf("Comando desconhecido\n");
+            break;
     }
     printa_celula();
     imprime_cursor();
@@ -294,12 +416,20 @@ int parse(celula* cel, console* cons) {
     while(tail->next != NULL)
         tail = tail->next;
     
-    // passo recursivo (executa próximas funções diferentes de 'I')
-    if(check_vec(cons->letraz[1], 0) == true){
-        // printf("%d",cons->letraz[1]);
-        if(c != 'I')    
-            parse(cel, aux);
+    // passo recursivo: casos expurios
+    if(check_vec(cons->letraz[1], 0) == false){
+        free(aux);
+        return 0;
     }
+    if(aux->tamanho == 0){
+        free(aux);
+        return 0;
+    }
+    if(c != 'I'){
+        parse(cel, aux);
+    }
+    if(aux != NULL)
+        free(aux);
     return 0;
 }
 
@@ -332,4 +462,6 @@ int main(){
 
     return 0;
 }
+
+
 
